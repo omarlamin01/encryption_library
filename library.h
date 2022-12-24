d97c9978322a0a14665e45d3e0f3a5f6041cbbc6
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-12-24 21:29:09 +0100

    sime changes

--- library.h
+++ library.h
@@ -12,6 +12,13 @@ unsigned modulo(int value, unsigned m) {
     return mod;
 }
 
+int modInverse(int A, int M)
+{
+    for (int X = 1; X < M; X++)
+        if (((A % M) * (X % M)) % M == 1)
+            return X;
+}
+
 std::string ceasar_encrypt(std::string message, int key) {
     std::string encrypted_message = "";
     for (int i = 0; i < message.length(); i++) {
@@ -36,38 +43,24 @@ bool ceasar_cipher(std::string encrypted_message, std::string decrypted_message,
     return ceasar_encrypt(decrypted_message, key) == encrypted_message;
 }
 
-std::string affine_cipher(int key1, int key2, std::string message, int mode) {
-    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-    switch (mode) {
-        case 0: {
-            std::string encrypted_message = "";
-            message = message;
-            for (int i = 0; i < message.length(); i++) {
-                int index = alphabet.find(tolower(message[i]));
-                int new_index = (key1 * index + key2) % 26;
-                encrypted_message += alphabet[new_index];
-            }
-            return encrypted_message;
-        }
-
-        case 1: {
-            std::string decrypted_message = "";
-            message = message;
-            for (int i = 0; i < message.length(); i++) {
-                int index = alphabet.find(tolower(message[i]));
-
-                int new_index = (26 + (index - key2) * key1) % 26;
-                decrypted_message += alphabet[new_index];
-            }
-            return decrypted_message;
-        }
-
-        default: {
-            std::cout << "Invalid mode" << std::endl;
-            return NULL;
-        }
+std::string affine_encrypt(std::string message, int key1, int key2) {
+    std::string encrypted_message = "";
+    for (int i = 0; i < message.length(); i++) {
+        int index = int(message[i]);
+        int new_index = modulo((key1 * index + key2), 255);
+        encrypted_message += char(new_index);
     }
+    return encrypted_message;
+}
+
+std::string affine_decrypt(std::string message, int key1, int key2) {
+    std::string decrypted_message = "";
+    for (int i = 0; i < message.length(); i++) {
+        int index = int(message[i]);
+        int new_index = modulo((key1 * (index - key2)), 255);
+        decrypted_message += char(new_index);
+    }
+    return decrypted_message;
 }
 
 std::string vigenere_cipher(std::string key, std::string message, int mode) {
