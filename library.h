b34cb2a7d34e596ebbb58162e628265ecf5ab9da
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-12-29 22:21:52 +0100

    vigenere cipher

--- library.h
+++ library.h
@@ -72,4 +72,40 @@ std::string affine_cipher(int key1, int key2, std::string message, int mode) {
     }
 }
 
+std::string vigenere_cipher(std::string key, std::string message, int mode) {
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    switch (mode) {
+        case 0: {
+            std::string encrypted_message = "";
+            message = message;
+            for (int i = 0; i < message.length(); i++) {
+                int index = alphabet.find(tolower(message[i]));
+                int key_index = alphabet.find(tolower(key[i % key.length()]));
+                int new_index = (index + key_index) % 26;
+                encrypted_message += alphabet[new_index];
+            }
+            return encrypted_message;
+        }
+
+        case 1: {
+            std::string decrypted_message = "";
+            message = message;
+            for (int i = 0; i < message.length(); i++) {
+                int index = alphabet.find(tolower(message[i]));
+                int key_index = alphabet.find(tolower(key[i % key.length()]));
+
+                int new_index = (26 + (index - key_index)) % 26;
+                decrypted_message += alphabet[new_index];
+            }
+            return decrypted_message;
+        }
+
+        default: {
+            std::cout << "Invalid mode" << std::endl;
+            return NULL;
+        }
+    }
+}
+
 #endif //ENCRYPTION_LIBRARY_LIBRARY_H
